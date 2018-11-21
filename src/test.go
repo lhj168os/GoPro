a6afff5908600fe29cac15dff21b858cd541dcbf
jay <lhj168os@gmail.com>
2018-11-21 14:03:18 +0800

    create project

---
+++ src/test.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+var pipe chan int
+
+func test(num1 int, num2 int) {
+	for i := num1; i < num2; i++ {
+		pipe <- i
+	}
+}
+
+func main() {
+	pipe = make(chan int, 3)
+	go test(0, 10)
+	go test(20, 30)
+	go test(50, 60)
+	go test(101, 122)
+	for {
+		num := <-pipe
+		fmt.Println(num)
+		if len(pipe) <= 0 {
+			break
+		}
+		time.Sleep(time.Second)
+	}
+}
