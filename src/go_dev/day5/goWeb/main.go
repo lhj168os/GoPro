31eabd943c247465aa051b7522a8657505682562
jay <lhj168os@gmail.com>
2018-12-06 02:02:22 +0800

    goweb 登录

--- src/go_dev/day5/goWeb/main.go
+++ src/go_dev/day5/goWeb/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"log"
 	"net/http"
 	"strings"
@@ -20,8 +21,21 @@ func sayhelloName(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello world!")
 }
 
+func login(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("method: ", r.Method)
+	if r.Method == "GET" {
+		t, _ := template.ParseFiles("login.gtpl")
+		t.Execute(w, nil)
+	} else {
+		r.ParseForm()
+		fmt.Println("username: ", r.Form["username"])
+		fmt.Println("password: ", r.Form["password"])
+	}
+}
+
 func main() {
 	http.HandleFunc("/", sayhelloName)
+	http.HandleFunc("/login", login)
 	err := http.ListenAndServe(":9090", nil)
 	if err != nil {
 		log.Fatal("ListenAndServer: ", err)
